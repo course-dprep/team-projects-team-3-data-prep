1305037c0a55f9a174c6a0f8bf9424f1d95ad02c
WillemvdMerbel <W.n.vdmerbel@tilburguniversity.edu>
2026-02-18 12:03:01 +0100

    Adding only the part that programmatically loads data

--- data/download-data.R
+++ data/download-data.R
@@ -1,2 +1,15 @@
 # This script will be used to populate the \data directory
 # with all necessary raw data files.
+
+
+#Below is the code to download the relevant yelp dataset, note that you need to choose 1 or 2 in the console to login to google drive to get the authorization to download the data
+
+
+suppressWarnings(library(googledrive))
+file_id <- "13AZqPcwUro0jwsZIv6Q3WXeEn58YD5_x" 
+drive_deauth()  # clear old token
+drive_auth(scopes = "drive.readonly")
+drive_download(as_id(file_id), path = "users.csv", overwrite = TRUE)
+library(tidyverse)
+Dataset <- read_csv("users.csv")
+View(Dataset)
